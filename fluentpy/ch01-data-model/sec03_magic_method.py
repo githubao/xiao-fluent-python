b1ceef010dc6e328557fe28cac65f014e5479fbd
BaoQiang <mailbaoqiang@gmail.com>
2017-06-09 17:55:46 +0800

    init project

---
+++ fluentpy/ch01-data-model/sec03_magic_method.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@description: 所有的magic method
+
+@author: BaoQiang
+@time: 2017/6/9 17:55
+"""
+
+
+def main():
+    print('do sth')
+
+
+if __name__ == '__main__':
+    main()
---
+++ fluentpy/tmp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@description: 测试类
+
+@author: BaoQiang
+@time: 2017/6/9 17:52
+"""
+import math
+
+
+def tmp():
+    print(math.hypot(3, 4))
+
+
+def main():
+    tmp()
+
+
+if __name__ == '__main__':
+    main()
