ed8289bf7127bc640cbc7cb7457432ea434b2bbd
BaoQiang <mailbaoqiang@gmail.com>
2017-06-14 18:19:29 +0800

    pythonic seq class

--- fluentpy/ch10-seq-hacking/pythonic_seqclass.py
+++ fluentpy/ch10-seq-hacking/pythonic_seqclass.py
@@ -11,10 +11,15 @@
 from array import array
 import math
 import reprlib
+import numbers
+import functools
+import operator
+import itertools
 
 
 class Vector:
     typecode = 'd'
+    shortcut_names = 'xyzt'
 
     def __init__(self, components):
         self._components = array(self.typecode, components)
@@ -40,7 +45,18 @@ class Vector:
         return (bytes([ord(self.typecode)]) + bytes(array(self.typecode, self)))
 
     def __eq__(self, other):
+        return len(self) == len(other) and all(a == b for a, b in zip(self, other))
+
+    def __eq2__(self, other):
         return tuple(self) == tuple(other)
+
+    def __eq3__(self, other):
+        if len(self) != len(other):
+            return False
+        for a, b in zip(self, other):
+            if a != b:
+                return False
+        return True
 
     def __abs__(self):
         return math.sqrt(sum(x * x for x in self))
@@ -48,9 +64,87 @@ class Vector:
     def __bool__(self):
         return bool(abs(self))
 
+    def __len__(self):
+        return len(self._components)
+
+    def __getitem__(self, item):
+        cls = type(self)
+        if isinstance(item, slice):
+            return cls(self._components[item])
+        elif isinstance(item, numbers.Integral):
+            return self._components[item]
+        else:
+            msg = '{cls.__name__} indices must be integers'
+            raise TypeError(msg.format(cls=cls))
+
+    def __getattr__(self, item):
+        cls = type(self)
+        if len(item) == 1:
+            pos = cls.shortcut_names.find(item)
+            if 0 <= pos < len(self._components):
+                return self._components[pos]
+
+        msg = '{.__name__!r} object has no attribute {!r}'
+        raise AttributeError(msg.format(cls, item))
+
+    def __setattr__(self, key, value):
+        cls = type(self)
+        if len(key) == 1:
+            if key in cls.shortcut_names:
+                error = 'readonly attribute {attr_name!r}'
+            elif key.lower():
+                error = 'can not set attribute a to z in {cls_name!r}'
+            else:
+                error = ''
+            if error:
+                msg = error.format(cls_name=cls.__name__, attr_name=key)
+                raise AttributeError(msg)
+
+        super().__setattr__(key, value)
+
+    def __hash__(self):
+        hashes = (hash(x) for x in self._components)
+        return functools.reduce(operator.xor, hashes, 0)
+
+    def angle(self, n):
+        r = math.sqrt(sum(x * x for x in self[n:]))
+        a = math.atan2(r, self[n - 1])
+        if (n == len(self) - 1) and (self[-1] < 0):
+            return math.pi * 2 - a
+        else:
+            return a
+
+    def angles(self):
+        return (self.angle(n) for n in range(1, len(self)))
+
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('h'):
+            fmt_spec = fmt_spec[:-1]
+            coords = itertools.chain([abs(self)], self.angles())
+            outer_fmt = '<{}>'
+        else:
+            coords = self
+            outer_fmt = '({})'
+
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(', '.join(components))
+
+
+def vec_cls():
+    v1 = Vector([3, 4, 5])
+    # print(v1[2])
+    # print(v1[:])
+    # print(v1[1, 2])
+
+    print(v1.y)
+    v1.next = 3
+    print(v1.next)
+
+    print(format(v1, '0.4fh'))
+
 
 def main():
-    print('do sth')
+    vec_cls()
 
 
 if __name__ == '__main__':
